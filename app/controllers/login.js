a2e02cd81447eccc4b5b3a74abe59295d0adcd99
Diego Costa <diego.machado.d.costa@gmail.com>
2019-03-22 19:44:56 +0000

    estrutura

---
+++ app/controllers/login.js
@@ -0,0 +1,3 @@
+module.exports.render = (res) => {
+    res.render('login');
+}
---
+++ config/config.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const consign = require('consign');
+const expressValidator = require('express-validator');
+const bodyParser = require('body-parser');
+const minifyHTML = require('express-minify-html');
+
+const app = express();
+
+
+app.set('view engine', 'ejs');
+app.set('views', 'app/views');
+
+app.use(express.static('public'));
+
+app.use(minifyHTML({
+    override: true,
+    exception_url: false,
+    htmlMinifier: {
+        collapseBooleanAttributes: true,
+        removeComments: true,
+        collapseWhitespace: true,
+        removeAttributeQuotes: true,
+        removeEmptyAttributes: true,
+        minifyJS: true
+    }
+}));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(expressValidator());
+
+consign()
+    .include('app/routes')
+    .then('app/controllers')
+    .then('app/models')
+    .into(app);
+
+module.exports = app;
